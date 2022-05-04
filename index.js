04d5f2f197a8b627bfa19a7ee0d60975f9618656
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2022-05-03 23:50:38 -0500

    uso de variables

---
+++ index.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+
+const nombre = 'Francisco';
+const apellido = 'Santiago';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado )
+
+//var No se recomienda su uso
+if ( true ) {
+    const nombre = 'Peter';
+    console.log(nombre)
+}
+
+console.log( valorDado );
